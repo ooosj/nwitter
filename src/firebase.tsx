74e403afa22b1e5c0732e14a4e21a90cda968f48
ooosj <tmdwn8775@gmail.com>
2024-05-11 23:09:20 +0900

    firebase 연동

---
+++ src/firebase.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSMBGT8OOu_y0U1x8lwstRIpKIrpGkBQA",
+  authDomain: "nwitter-61baa.firebaseapp.com",
+  projectId: "nwitter-61baa",
+  storageBucket: "nwitter-61baa.appspot.com",
+  messagingSenderId: "599029429084",
+  appId: "1:599029429084:web:20510ce8de274f497c4799"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
